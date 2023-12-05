1eac3ae7561e80a300ef8d3182beeeab114a893d
Dennis Zelada <denniszelada@gmail.com>
2023-12-05 14:20:35 +0100

    Add scrapper function with beautiful soup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from langchain.chat.models import ChatOpanAI
 from langchain.prompts import ChatPromptTemplate
+import requests
+from bs4 import BeautifulSoup
 
 template = """Summarize the following question based on the context:
 
@@ -10,5 +12,23 @@ Context
 {context}"""
 
 prompt = ChatPromptTemplate.from_template(template)
+
+def scrape_text(url: str):
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, "html.parser")
+
+            page_text = soup.get_text(separator=" ", strip=True)
+
+            return page_text
+        else:
+            return f"Failed to retrieve the webpage: Status code {response.status.code}"
+    except Exception as e:
+        print(e)
+        return f"Failed to retrieve the webpage: {e}"
+    
+
 
 url = "https://blog.langchain.dev/announcing-langsmith/"
